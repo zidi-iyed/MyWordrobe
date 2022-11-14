a6fc1928f1226671eccea7d737bc25bd8319e19e
zidi-iyed <iyed.zidi@esprit.tn>
2022-11-14 17:47:35 +0100

    add layout

--- app/src/main/java/tn/esprit/mywardrobe/MainActivity.kt
+++ app/src/main/java/tn/esprit/mywardrobe/MainActivity.kt
@@ -2,10 +2,17 @@ package tn.esprit.mywardrobe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tn.esprit.mywardrobe.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //injecter le fragment dans notre boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
